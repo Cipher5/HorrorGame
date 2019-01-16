213ea29330054fe7e0afc6cd0233000ea6bafb26
mvcodeclub <sarah@mvcodeclub.com>
2019-01-15 17:54:42 -0800

    Started plane explosion

---
+++ HorrorGame/Assets/Scripts/playerController.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class playerController : MonoBehaviour {
+
+	public float speed = 10f;
+	public GameObject brokenPlane;
+	Rigidbody rb;
+	// Use this for initialization
+	void Start () {
+		rb = this.GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		rb.AddForce (Vector3.forward * speed);
+
+
+	}
+	void OnTriggerEnter(Collider other){ 
+		
+		if (other.gameObject.tag == "CrashTrigger") {
+			this.gameObject.SetActive (false);
+			Instantiate (brokenPlane, this.transform.position, this.transform.rotation, null);
+		}
+	}
+}
+	
